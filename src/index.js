27de289f8b38769a660275e7c579e90e55d23f42
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-11-18 14:00:53 +0300

    Completed assignments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/* Add NavLink to import */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+/* add the NavBar component to our render method */
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar /> 
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
